fbe370c24ec4708b4f9543301af52f5b26c180fc
Ravindu Bandara Subasinha <it22098450@my.sliit.lk>
2025-04-30 23:34:26 +0530

    update css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,11 @@
-
-import './App.css'
+import "./App.css";
 
 function App() {
-  
-
   return (
-    <div className='bg-'>
-      <h1>Welcome to the React App</h1>
+    <div>
+      <h1 className="">Welcome to the React App</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
